ede38916b26a42625b498bfe1d772534652a102e
Gabor Pfeiffer <gaborpfeiffer221@gmail.com>
2025-12-29 17:28:22 +0100

    add nix features

--- common/packages.nix
+++ common/packages.nix
@@ -3,6 +3,11 @@
 {
   nixpkgs.config.allowUnfree = true;
 
+    nix.settings.experimental-features = [
+    "nix-command"
+    "flakes"
+  ];
+
   environment.systemPackages = with pkgs; [
     vim
     google-chrome
